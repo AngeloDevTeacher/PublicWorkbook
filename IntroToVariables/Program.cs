55cc8c28bb48d3d394256284ffdbfdd3389740b9
angebl0w <encarnacion.angelo@gmail.com>
2024-09-10 11:47:01 -0600

    Created intro to var, declared number types

---
+++ IntroToVariables/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Purpose: Show different variables.
+/// Author: Angelo Encarnacion angeloe@nait.ca
+/// Date Modified: 2024-09-10
+/// </summary>
+/// 
+
+// There are mutliple number type of variables:
+// int, float, double, decimal, and more!
+// We will use DOUBLE for most of the class.
+
+
+// ints are whole numbers
+int thisIsAnInt = 12;
+
+// Floats have a "f" at the end of the number.
+float thisIsAFloat = 12.4f;
+
+// Doubles do NOT have a unit at the end of a number.
+double thisIsADouble = 12.4;
+
+// Decimals have a "m" at the end of the number
+decimal thisIsADecimal = 12.4m;
+
