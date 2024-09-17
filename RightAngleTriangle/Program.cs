32e0775fd4b687d2cb724ed3b9f1e0e7107e8f18
angebl0w <encarnacion.angelo@gmail.com>
2024-09-17 11:14:15 -0600

    Created variables

---
+++ RightAngleTriangle/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// Write a program that solves for the hypotenuse given the height and width of a right angle triangle.
+/// Author: Angelo Encarnacion - angeloe@nait.ca
+/// Date Modified: 2024-09-17
+/// </summary>
+/// 
+
+double height = 0;
+double width = 0;
+
+double hypotenuse = 0;
