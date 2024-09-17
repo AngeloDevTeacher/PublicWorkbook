201305df443c213aeb89f5e370bb9c123401ca8b
angebl0w <encarnacion.angelo@gmail.com>
2024-09-17 11:25:45 -0600

    output

--- RightAngleTriangle/Program.cs
+++ RightAngleTriangle/Program.cs
@@ -22,3 +22,11 @@ width = double.Parse(Console.ReadLine());
 
 Console.Write("Enter the height of the triangle: ");
 height = double.Parse(Console.ReadLine());
+
+// Set the value of the hypotenuse to the formula.
+hypotenuse = Math.Sqrt( Math.Pow(width,2) + Math.Pow(height,2) );
+
+// Output the result. If I round in the output I keep the accurate value in memory in case I have logic later.
+Console.WriteLine();
+Console.WriteLine($"The hypotenuse is {Math.Round(hypotenuse,5)}");
+Console.WriteLine("END OF PROGRAM");
