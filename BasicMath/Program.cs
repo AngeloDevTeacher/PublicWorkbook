28e456d92ceb49c6bab3a682f6910691b588a3ec
angebl0w <encarnacion.angelo@gmail.com>
2024-09-17 11:05:26 -0600

    Cookie calories

---
+++ BasicMath/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Do addition, subtraction, multiplication, and division.
+// 2. Do multiple of the them in the same line.
+// 3. TEST BEDMAS - does it work?
+// 4. Adv: Test the Math. fucntions, (example Math.Pow())
+
+
+float numberOfSocks = 256;
+float cats = 2;
+float dogs = 4;
+numberOfSocks = numberOfSocks - 4;
+
+numberOfSocks -= 4;
+
+numberOfSocks /= 5;
+
+Console.WriteLine($"The sum of cats and dogs is {cats + dogs}");
---
+++ GirlScoutCookies/Program.cs
@@ -0,0 +1,40 @@
+﻿///<summary>
+/// This program calculates the amount of money made from selling cookies.
+/// Author: Angelo Encarncaion - angeloe@nait.ca
+/// Date Modified: 2024-09-17
+/// </summary>
+/// 
+
+
+/* 
+ * A bag of cookies holds 26/10 cookies.
+ * The calorie information on the bag claims that there are 10
+servings in the bag and that each serving equals 175 calories.
+Write a program that lets the user
+enter the number of cookies he or she actually ate and then reports the number of total calories
+consumed 
+ */
+
+int bagCapacity = 26;
+int bagServingCount = 10;
+
+// This will be 2.6f
+float cookiesPerServing = (float)bagCapacity / (float)bagServingCount;
+int caloriesPerServing = 175;
+
+int cookiesEaten = 0;
+float caloriesEaten = 0;
+
+
+// Display Program intro
+Console.WriteLine("--- Calorie intake calculator ---");
+Console.WriteLine();
+
+// Prompt for user to enter the amount of cookies:
+Console.Write("Enter a whole number of cookie eaten: ");
+cookiesEaten = int.Parse(Console.ReadLine());
+
+caloriesEaten = (caloriesPerServing / cookiesPerServing) * cookiesEaten;
+
+Console.WriteLine("You have eaten " + caloriesEaten + " calories");
+Console.ReadLine();
