e1dc545b2457a5077fbd0eca11171104dcb6040e
angebl0w <encarnacion.angelo@gmail.com>
2024-09-19 10:23:17 -0600

    Made my prompts

--- AlexQuestions/Program.cs
+++ AlexQuestions/Program.cs
@@ -18,10 +18,18 @@ string userInput = "";
 // Main Program
 
 // Display program purpose
+Console.WriteLine("------ Cookie cost calculator ------");
+Console.WriteLine();
 
 // Prompt for amount of cookies
+Console.Write("Enter a whole number for the amount of cookies:\t");
+userInput = Console.ReadLine();
+amountOfCookies = int.Parse(userInput);
 
 // Prompt for cost per cookie
+Console.Write("Enter in a cost per cookie:\t");
+userInput = Console.ReadLine();
+costPerCookie = double.Parse(userInput);
 
 // DO MATH - calculate the totalCost
 
