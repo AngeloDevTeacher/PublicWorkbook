671581df031eeaef189c3ed0b833c3f95f44d0c1
angebl0w <encarnacion.angelo@gmail.com>
2024-09-19 10:17:47 -0600

    Created Program, psudeocode

---
+++ AlexQuestions/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// Explore the variable and display questions.
+/// Author: Angelo Encarnacion angeloe@nait.ca
+/// Date Modified: 2024-09-19
+/// </summary>
+/// 
+
+
+// Variables
+int amountOfCookies;
+double costPerCookie;
+
+double totalCost;
+
+string userInput = "";
+
+
+// Main Program
+
+// Display program purpose
+
+// Prompt for amount of cookies
+
+// Prompt for cost per cookie
+
+// DO MATH - calculate the totalCost
+
+// Display totalCost
