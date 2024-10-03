522b8043d3154ae37dc942fe26a30031a4d53561
angebl0w <encarnacion.angelo@gmail.com>
2024-10-03 10:57:52 -0600

    Added else if examples

--- IntroToIf/Program.cs
+++ IntroToIf/Program.cs
@@ -22,7 +22,36 @@ bool haveDogs = false;
 bool haveBirds = false;
 bool haveBunnies = false;
 
-if (haveCats || haveDogs || haveBirds && haveBunnies)
+if ((haveCats || haveDogs || haveBirds) && haveBunnies)
 {
     Console.WriteLine("You are awesome");
+}
+
+else
+{
+    Console.WriteLine("This related code runs when above is FALSE");
+}
+
+bool isWendysOk = false;
+bool isPrimeTimeDonairOk = true;
+bool isMaplePizzaOk = true;
+string pattyShape = "circles";
+
+if (isWendysOk == true)
+{
+    Console.WriteLine("Yay! Let's go on a pilgrimage to Wendys!");
+    Console.WriteLine("And go go karting after");
+    pattyShape = "square";
+}
+else if (isPrimeTimeDonairOk == true)
+{
+    Console.WriteLine("Ok, let's go to Prime Time Donair");
+}
+else if (isMaplePizzaOk == true)
+{
+    Console.WriteLine("Sounds awesome, let's go!");
+}
+else
+{
+    Console.WriteLine("Dude. Just get something from the fridge, I guess.");
 }
