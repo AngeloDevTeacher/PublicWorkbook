14fa0d0490828b59ac48d5bce37e2af7d3a44763
angebl0w <encarnacion.angelo@gmail.com>
2024-10-10 10:40:13 -0600

    variables and summary.

--- Loops1/Program.cs
+++ Loops1/Program.cs
@@ -27,3 +27,25 @@ for (int i = 1; i <= mySquare; i++)
 // Display
 Console.WriteLine();
 Console.WriteLine($"The sum of the squares is {outputSum}");
+
+
+// Question 2:
+
+/// <summary>
+/// Add up inputted ages and output average.
+/// Author: Angelo Encarnacion
+/// Date modified: 2024-10-10
+/// </summary>
+
+// Variables
+// Total sum of age.
+int ageSum = 0;
+// Number of entries to be used in the average.
+int numberOfEntries = 0; 
+// The user input as a string.
+string ageInput = "";
+// The parsed value of the user input.
+int ageNumber = 0;
+
+// Program Start: 
+
