075df3d8a04a13a8d2eb0357e2ae321421787bc2
angebl0w <encarnacion.angelo@gmail.com>
2024-10-15 11:01:50 -0600

    Use a array with set length, logical size

--- IntroToArrays/Program.cs
+++ IntroToArrays/Program.cs
@@ -27,8 +27,44 @@ int MaxCatsPerHousehold = 5;
 string[] catsInAHousehold = new string[MaxCatsPerHousehold];
 
 // This is an example of efficient code for the commented section line 2 to line 9 above, using the array on line 19.
-for (int i = 0; i < angeloCatsNames.Length; i++)
+//for (int i = 0; i < angeloCatsNames.Length; i++)
+//{
+//    Console.WriteLine(angeloCatsNames[i].ToUpper());
+//}
+
+// Arrays have a SET SIZE after being set, it CANNOT change.
+// IF you try to change it, you are actually making a NEW array.
+// Variables
+int catsInHouse = 0;
+string userInput = "";
+// Program Purpose
+Console.WriteLine("--Cat Register--");
+Console.WriteLine();
+
+
+// We use a do-while loop to run at least ONCE.
+do
 {
-    Console.WriteLine(angeloCatsNames[i].ToUpper());
+    // Prompt
+    Console.WriteLine("Enter a cat name, or a blank entry to finish.");
+    Console.Write("Enter a cat name: ");
+    // Set the userInput to the ReadLine so we can use logic!
+    // We DON'T want to add an empty string OR increase the count IF the line was blank.
+    userInput = Console.ReadLine();
+    if (userInput != string.Empty)
+    {
+        // Set the current cat in the array.
+        // Remember, we are using a variable to keep track of the cats. We also will increment the variable on sucess.
+        catsInAHousehold[catsInHouse] = userInput;
+        catsInHouse++;
+    }
+    // We want to be able to exit the loop if the string was empty OR if we reached the max.
+} while (userInput != string.Empty && catsInHouse < MaxCatsPerHousehold);
+
+// We are using the "LOGICAL SIZE" - the size of the data we inputted.
+// If there are 3 cat names in the 5 length array, the array has a PHYSICAL SIZE/ length of 5, but a LOGICAL size of 3
+for (int i = 0; i < catsInHouse; i++)
+{
+    Console.WriteLine(catsInAHousehold[i]);
 }
 
