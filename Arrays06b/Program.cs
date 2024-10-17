2ed3346a42bb69116ba89754212b6fadaec0cc2e
angebl0w <encarnacion.angelo@gmail.com>
2024-10-17 10:21:22 -0600

    pseudocode and variables

---
+++ Arrays06b/Program.cs
@@ -0,0 +1,35 @@
+﻿/// <summary>
+/// Purpose: Enter in up to  25 whole positive numbers, using 0 to end input. Display the number set, and the smallest and largest number.
+/// Author: Angelo Encarnacion
+/// Last Modified: 2024-10-17
+/// </summary>
+/// 
+
+// Variables
+
+// This is the MAX SIZE
+const int NumbersMaxSize = 25;
+int[] numbers = new int[NumbersMaxSize];
+
+// This is the LOGICAL SIZE
+int countOfNumbersEntered = 0;
+
+int smallestNumber = 0;
+int largestNumber = 0;
+
+// Program START
+// Display purpose
+
+// do while
+    // Prompt the user to enter a number
+
+    // Validate and parse input
+        // Try catch
+        // Parse
+    // Comparisons for largest and smallest
+        // Update variables
+
+    // Display output
+
+
+// Program End
