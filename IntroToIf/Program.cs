3829bcf317eed895b7b12b1ff565002c6b9e35bc
angebl0w <encarnacion.angelo@gmail.com>
2024-10-22 11:38:42 -0600

    simple prompt with if

--- IntroToIf/Program.cs
+++ IntroToIf/Program.cs
@@ -5,69 +5,84 @@
 /// </summary>
 /// 
 
-int driversAge = 18;
-bool passedDriversTest = false;
+//int driversAge = 18;
+//bool passedDriversTest = false;
 
-if ((driversAge >= 18) && passedDriversTest)
+//if ((driversAge >= 18) && passedDriversTest)
+//{
+//    Console.WriteLine("You can drive.");
+//}
+//else
+//{
+//    Console.WriteLine("You cannot drive.");
+//}
+
+//bool haveCats = true;
+//bool haveDogs = false;
+//bool haveBirds = false;
+//bool haveBunnies = false;
+
+//if ((haveCats || haveDogs || haveBirds) && haveBunnies)
+//{
+//    Console.WriteLine("You are awesome");
+//}
+
+//else
+//{
+//    Console.WriteLine("This related code runs when above is FALSE");
+//}
+
+//bool isWendysOk = false;
+//bool isPrimeTimeDonairOk = true;
+//bool isMaplePizzaOk = true;
+//string pattyShape = "circles";
+
+//if (isWendysOk == true)
+//{
+//    Console.WriteLine("Yay! Let's go on a pilgrimage to Wendys!");
+//    Console.WriteLine("And go go karting after");
+//    pattyShape = "square";
+//}
+//else if (isPrimeTimeDonairOk == true)
+//{
+//    Console.WriteLine("Ok, let's go to Prime Time Donair");
+//}
+//else if (isMaplePizzaOk == true)
+//{
+//    Console.WriteLine("Sounds awesome, let's go!");
+//}
+//else
+//{
+//    Console.WriteLine("Dude. Just get something from the fridge, I guess.");
+//}
+
+//string studentName = "Angelo";
+
+//switch (studentName)
+//{
+//    case "Angelo":
+//        Console.WriteLine("HAHA I'M OT A STUDENT");
+//        break;
+
+//    case "Alex G":
+//        Console.WriteLine("I've been picked on!");
+//        break;
+//    default:
+//        Console.WriteLine("WTF");
+//        break;
+//}
+
+string userInput = "";
+
+Console.WriteLine("Your house [y] or your friends house [f]?\t");
+userInput = Console.ReadLine();
+
+if (userInput == "y")
 {
-    Console.WriteLine("You can drive.");
+    Console.WriteLine("YOUR HOUSE");
 }
-else
+else if (userInput == "f")
 {
-    Console.WriteLine("You cannot drive.");
+    Console.WriteLine("FRIENDS HOUSE");
 }
 
-bool haveCats = true;
-bool haveDogs = false;
-bool haveBirds = false;
-bool haveBunnies = false;
-
-if ((haveCats || haveDogs || haveBirds) && haveBunnies)
-{
-    Console.WriteLine("You are awesome");
-}
-
-else
-{
-    Console.WriteLine("This related code runs when above is FALSE");
-}
-
-bool isWendysOk = false;
-bool isPrimeTimeDonairOk = true;
-bool isMaplePizzaOk = true;
-string pattyShape = "circles";
-
-if (isWendysOk == true)
-{
-    Console.WriteLine("Yay! Let's go on a pilgrimage to Wendys!");
-    Console.WriteLine("And go go karting after");
-    pattyShape = "square";
-}
-else if (isPrimeTimeDonairOk == true)
-{
-    Console.WriteLine("Ok, let's go to Prime Time Donair");
-}
-else if (isMaplePizzaOk == true)
-{
-    Console.WriteLine("Sounds awesome, let's go!");
-}
-else
-{
-    Console.WriteLine("Dude. Just get something from the fridge, I guess.");
-}
-
-string studentName = "Angelo";
-
-switch (studentName)
-{
-    case "Angelo":
-        Console.WriteLine("HAHA I'M OT A STUDENT");
-        break;
-
-    case "Alex G":
-        Console.WriteLine("I've been picked on!");
-        break;
-    default:
-        Console.WriteLine("WTF");
-        break;
-}
