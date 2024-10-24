86ee938c8eea6eaf2dec3e90175f821aa76c0c76
angebl0w <encarnacion.angelo@gmail.com>
2024-10-24 10:37:03 -0600

    variables

--- PetCalc/Program.cs
+++ PetCalc/Program.cs
@@ -21,8 +21,20 @@
 
 
 // Variables
+int amountOfMonths = 0;
+string typeOfPet = ""; // My only valid answers will be "c" or "d".
 
+Random randomInstance = new Random();
+double addtionalCostPerMonth = randomInstance.Next(3000, 5001)/100.00; // Lets say I had to calculate between $30 and 50
+//Console.WriteLine( $"{addtionalCostPerMonth:c}"); // Currency format!
 
+const double CatLicenseCost = 20.00;
+const double DogLicenseCost = 120.00;
+
+const double FoodRateUnderTwoYears = 500.00;
+const double FoodRateFromTwoYearsToEight = 900.00;
+const double FoodRateFromEightYearsToTwelve = 1200.00;
+const double FoodRatePerMonthAdditional = 20.00;
 // Main Program
 
 // End Program
