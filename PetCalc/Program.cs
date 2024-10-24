74d2974654ec6390a7e99a8dab0da76395744d70
angebl0w <encarnacion.angelo@gmail.com>
2024-10-24 10:18:17 -0600

    summary

--- PetCalc/Program.cs
+++ PetCalc/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// <summary>
+/// Purpose: To generally caluculate a pet's cost of living by pet and number of months.
+/// Author: Angelo Encarnacion angeloe@nait.ca
+/// Date Modified: 2024-10-24
+/// </summary>
