99e317c79acefb96afa916ebf608d79fb23a4247
angebl0w <encarnacion.angelo@gmail.com>
2024-10-24 10:22:10 -0600

    Pseudo-code

--- PetCalc/Program.cs
+++ PetCalc/Program.cs
@@ -3,3 +3,26 @@
 /// Author: Angelo Encarnacion angeloe@nait.ca
 /// Date Modified: 2024-10-24
 /// </summary>
+/// 
+
+// General Plan :
+// Calculate random
+// Init variables
+// Loop-Prompt the user, until I get valid answers
+// Calc:
+// - License costs PER FULL YEAR
+// - Misc cost per month
+// - Food Costs per MONTH
+
+// Display results
+// Use alignment formatting!
+// Ensure using currency format.
+
+
+
+// Variables
+
+
+// Main Program
+
+// End Program
