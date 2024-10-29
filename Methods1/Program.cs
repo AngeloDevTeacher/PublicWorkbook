4499bdd6fb9b5c50b0aea74ac2f5b0c6e61857a5
angebl0w <encarnacion.angelo@gmail.com>
2024-10-29 11:34:43 -0600

    implementation

--- Methods1/Program.cs
+++ Methods1/Program.cs
@@ -8,9 +8,22 @@
 /// </summary>
 /// 
 
+double myMoney = 0;
+myMoney = GetTotal();
+Console.WriteLine($"{myMoney:c}");
+
 double GetTotal()
 {
     double output = 0;
+    int pennies, nickels, dimes, quarters, loonies, twoonies;
+    pennies = GetCoin("How many pennies do you have?\t");
+    nickels = GetCoin("How many nickels do you have?\t");
+    dimes = GetCoin("How many dimes do you have?\t");
+    quarters = GetCoin("How many quarters do you have?\t");
+    loonies = GetCoin("How many loonies do you have?\t");
+    twoonies = GetCoin("How many twoonies do you have?\t");
+
+    output = (pennies * 0.01) + (nickels * 0.05) + (dimes * 0.10) + (quarters * 0.25) + (loonies * 1.00) + (twoonies * 2.00);
 
     return output;
 }
@@ -48,5 +61,4 @@ int GetCoin(string textCoinPrompt)
     return coin;
 }
 
-int test = GetCoin("Enter in the amount of pennies\t");
-Console.WriteLine(test);
+
