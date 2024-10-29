aece5e9028903514124cb150bf74ce14c98ef5cf
angebl0w <encarnacion.angelo@gmail.com>
2024-10-29 10:44:19 -0600

    overloaded methods, methods 2 with scope

---
+++ Methods2/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Learning about variable scope and multiple parameters.
+/// Author: Angelo Encarnacion
+/// Date Modified: 2024-10-29
+/// </summary>
+/// 
+
+string name = "Angelo Encarnacion";
+
+
+string ExclaimCatName(string temporaryCatNamePlaceholder)
+{
+    return temporaryCatNamePlaceholder+"!";
+}
+
+void MakePeanutButterAndJellySandwich(string slice1, string slice2)
+{
+
+}
+
+
+
+Console.WriteLine(ExclaimCatName("Sully"));
+
---
+++ OverloadedMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace OverloadedMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = Prompt("Hi Class!", ConsoleColor.DarkMagenta);
+            Console.WriteLine(output);
+        }
+        static string Prompt(string text)
+        {
+            Console.Write(text);
+            return Console.ReadLine();
+        }
+        static string Prompt(string text, ConsoleColor color)
+        {
+            Console.Write(text);
+            Console.ForegroundColor = color;
+            return Console.ReadLine();
+        }
+    }
+}
