a394423ad4940bb32fafc07d2de05349f8cddc0f
angebl0w <encarnacion.angelo@gmail.com>
2024-10-31 10:17:55 -0600

    Rainbow question

---
+++ MethodsWithArrays/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] rainbowColors = { "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet" };
+
+string[] myCopyOfRainbow;
+
+int cash = 100;
+int myCash = 0;
+
+myCash = cash;
+myCash += 1;
+
+myCopyOfRainbow = rainbowColors;
+for (int i = 0; i < 7; i++)
+{
+    myCopyOfRainbow[i] = myCopyOfRainbow[i].ToUpper();
+    Console.WriteLine(rainbowColors[i]);
+}
