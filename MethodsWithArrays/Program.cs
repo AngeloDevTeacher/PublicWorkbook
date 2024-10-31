6153b67f8552a185fd914f7da1f62cdb61fba55c
angebl0w <encarnacion.angelo@gmail.com>
2024-10-31 10:27:51 -0600

    Show CopyTo

--- MethodsWithArrays/Program.cs
+++ MethodsWithArrays/Program.cs
@@ -7,8 +7,13 @@ int myCash = 0;
 
 myCash = cash;
 myCash += 1;
+// This line sets myCopy's address to the same as rainbowColors.
+//myCopyOfRainbow = rainbowColors;
 
-myCopyOfRainbow = rainbowColors;
+// Do this to make a new set of data to edit! This also sets myCopy to that address, so I won't have the same mistake!
+myCopyOfRainbow = new string[rainbowColors.Length];
+rainbowColors.CopyTo(myCopyOfRainbow, 0);
+
 for (int i = 0; i < 7; i++)
 {
     myCopyOfRainbow[i] = myCopyOfRainbow[i].ToUpper();
