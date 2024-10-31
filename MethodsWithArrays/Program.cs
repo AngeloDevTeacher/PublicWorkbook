38c5c040afac842295177942c70767803ba0d13c
angebl0w <encarnacion.angelo@gmail.com>
2024-10-31 11:33:50 -0600

    ref vs value

--- MethodsWithArrays/Program.cs
+++ MethodsWithArrays/Program.cs
@@ -19,3 +19,37 @@ for (int i = 0; i < 7; i++)
     myCopyOfRainbow[i] = myCopyOfRainbow[i].ToUpper();
     Console.WriteLine(rainbowColors[i]);
 }
+
+// Methods with arrays.
+const int Grades_Max_Size = 10;
+int[] grades = new int[Grades_Max_Size];
+
+grades[0] = 12;
+grades[1] = 26;
+
+static int[] AddGrades(int[] gradesToChange, int amount)
+{
+    int[] output = new int[gradesToChange.Length];
+    for (int i = 0; i < gradesToChange.Length; i++)
+    {
+        gradesToChange[i] += amount;
+        output[i] = gradesToChange[i];
+    }
+    return output;
+}
+
+AddGrades(grades, 6);
+Console.WriteLine();
+
+
+int catCount = 3;
+int newCount = 0;
+static int AddCats(int currentCount, int addedNumber)
+{
+    currentCount += addedNumber;
+    return currentCount;
+}
+
+newCount = AddCats(catCount, 1);
+Console.WriteLine(newCount);
+Console.WriteLine(catCount);
