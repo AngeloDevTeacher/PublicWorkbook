e8dbcdf65740c0df4d6b503402906bf3dfae15c0
angebl0w <encarnacion.angelo@gmail.com>
2024-11-04 10:52:51 -0700

    i forgot to commit sorry

---
+++ IntroToWriting/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// These are the very basic patterns of reading and writing files.
+string fileName = "test.csv";
+bool appendToFile = true;
+
+StreamWriter writer = new StreamWriter($"../../../{fileName}", appendToFile);
+writer.WriteLine("Bulbasaur, Lvl 21");
+writer.WriteLine("Pikachu, Lvl 2");
+writer.Close();
+
+StreamReader reader = new StreamReader($"../../../{fileName}");
+string line; 
+for (int i = 0; i < 6; i++)
+{
+    line = reader.ReadLine();
+    Console.WriteLine(line);
+}
+reader.Close();
