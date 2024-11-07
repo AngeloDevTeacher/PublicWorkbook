c9a194b3079c2b571c7639c24888db3a1cf66312
angebl0w <encarnacion.angelo@gmail.com>
2024-11-07 10:31:27 -0700

    Created input loop

--- AdvancedArrays/Program.cs
+++ AdvancedArrays/Program.cs
@@ -18,10 +18,53 @@ const int NumberArraySize = 100;
 int[] numbers = new int[NumberArraySize];
 int logicalSize = 0;
 
-string userInput = "";
+
 int numberInput = 0;
 
 do
 {
-
+	numberInput = PromptInt("Please enter a whole positive number, use -1 to stop:\t");
+	if(numberInput == EndInput)
+	{
+        Console.WriteLine("Stopping input");
+    }
+	else if (numberInput < 0)
+	{
+        Console.WriteLine("Please enter a positive number");
+    }
+	else
+	{
+		numbers[logicalSize] = numberInput;
+		logicalSize++;
+	}
 } while (numberInput != EndInput && logicalSize < NumberArraySize);
+
+for (int i = 0; i < logicalSize; i++) 
+{
+	Console.WriteLine(numbers[i]);
+}
+
+
+
+/// <summary>
+/// Prompt the user using message, <see langword="return"/> their input if it's an int.
+/// </summary>
+static int PromptInt(string message)
+{
+	int output = 0;
+	bool isValid = false;
+	do
+	{
+		try
+		{
+			Console.Write(message);
+			output = int.Parse(Console.ReadLine());
+			isValid = true;
+		}
+		catch (Exception)
+		{
+            Console.WriteLine("Error: Please enter a whole number.");
+        }
+	} while (!isValid);
+	return output;
+}
