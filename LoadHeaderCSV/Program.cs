2f4160bf2ea679e2855e4f0ed597c69fab994de1
angebl0w <encarnacion.angelo@gmail.com>
2024-11-18 10:35:52 -0700

    Skipped header

---
+++ LoadHeaderCSV/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// GOAL: Read a CSV with header data in the top row.
+/// Author: Angelo Encarnacion
+/// Date Modified: 2024/11/18
+/// </summary>
+/// 
+const int MaxSize = 25;
+string fileName = "test.csv";
+int count = 0;
+
+string[] pokemonName = new string[MaxSize];
+int[] pokemonLevel = new int[MaxSize];  
+
+StreamReader reader = new StreamReader($"../../../{fileName}");
+string line;
+while (!reader.EndOfStream)
+{
+    line = reader.ReadLine();
+    if(count > 0)
+    {
+        string[] items = line.Split(",");
+
+        pokemonName[count-1] = items[0];
+        pokemonLevel[count-1] = int.Parse(items[1]);
+    }
+    count++;
+    //Console.WriteLine(line);
+}
+reader.Close();
+Console.WriteLine("DONE");
