3cd7d85ee3703264b320101b509d91c5f1fd060c
angebl0w <encarnacion.angelo@gmail.com>
2024-11-18 10:23:00 -0700

    Date and Time

---
+++ DateTime/Program.cs
@@ -0,0 +1,10 @@
+﻿DateTime today = DateTime.Now;
+
+Console.WriteLine(today.DayOfWeek);
+
+string dateToParse = "";
+Console.Write("Enter a date in this format(MM/DD/YYYY):\t");
+dateToParse = Console.ReadLine();
+
+DateTime parsedDate = DateTime.Parse(dateToParse);
+Console.WriteLine(parsedDate);
