38aa0fc18858c77de92942ef72b96471c491098a
angebl0w <encarnacion.angelo@gmail.com>
2024-11-26 10:59:14 -0700

    Vehicle

---
+++ OOPDay1/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+Book lotr = new Book();
+Book notw = new Book();
+Book twmf = new Book("The Wise Man's Fear", "rothfuss", 3);
+lotr.bookNum = 1;
+notw.bookNum = 2;
+notw.author = "rothfuss";
+lotr.author = "tolkien";
+
+Book[] bookshelf = new Book[3];
+bookshelf[0] = lotr;
+bookshelf[1] = notw;
+bookshelf[2] = twmf;
+foreach (Book book in bookshelf)
+{
+    Console.WriteLine(book.author);
+    Console.WriteLine(book.bookNum);
+}
+
+if (bookshelf[1].author == bookshelf[2].author)
+{
+
+}
+
+
+class Book
+{
+    public string title;
+    public string author;
+    public int bookNum;
+    string[] pages;
+
+
+
+    public Book()
+    {
+
+    }
+
+    public Book(string title, string author, int num)
+    {
+        this.title = title;
+        this.author = author;
+        this.bookNum = num;
+    }
+
+
+    /// <summary>
+    /// Returns the pages. 
+    /// </summary>
+    /// <returns>pages</returns>
+    string[] ReadBook()
+    {
+        return pages;
+    }
+    string ReadPage(int page)
+    {
+        return pages[page];
+    }
+}
---
+++ OOPDay2/Program.cs
@@ -0,0 +1,37 @@
+﻿Vehicle angelosCar = new Vehicle("Toyota", "Camry", 2004, "Engine", "Land", 4);
+Vehicle chrisSegway = new Vehicle("Segway", "miniPro", 2010, "Motors", "Land", 1);
+Vehicle jacobsTruck = new Vehicle("Chevy", "Silverado 1500", 2012, "Engine", "Land", 6);
+
+Vehicle[] classVehicles = { angelosCar, chrisSegway, jacobsTruck };
+
+foreach (Vehicle vehicle in classVehicles)
+{
+    Console.WriteLine(vehicle.modelName);
+}
+
+
+class Vehicle
+{
+    public string manufacturerName { get; private set; }
+    public string modelName { get; private set; }
+    public int year { get; private set; }
+
+    public string propulsion { get; private set; }
+    public string terrainPreference { get; private set; }
+    public int capacity { get; private set; }
+
+    public Vehicle()
+    {
+        modelName = "This needs a model name";
+    }
+
+    public Vehicle(string manufacturer, string model, int year, string propulsion, string terrain, int capacity)
+    {
+        manufacturerName = manufacturer;
+        modelName = model;
+        this.year = year;
+        this.propulsion = propulsion;
+        terrainPreference = terrain;
+        this.capacity = capacity;
+    }
+}
