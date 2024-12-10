9bb128024511746c285d6d91706200064e167827
angebl0w <encarnacion.angelo@gmail.com>
2024-12-10 11:05:03 -0700

    Started player scoreboard

---
+++ PlayerScoreboard/Player.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerScoreboard
+{
+    internal class Player
+    {
+        // Fields
+        string _firstName;
+        string _lastName;
+        int _playCount;
+        double _lifetimeScore;
+        // Properties
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _firstName = value.Trim();
+                }
+                else
+                {
+                    throw new Exception("First name cannot be null or empty");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _lastName = value.Trim();
+                }
+                else
+                {
+                    throw new Exception("Last name cannot be null or empty");
+                }
+            }
+        }
+
+        public int PlayCount
+        {
+            get
+            {
+                return _playCount;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("PlayCount must be 0 or greater");
+                }
+                else
+                {
+                    _playCount = value;
+                }
+            }
+        }
+
+        public double LifetimeScore
+        {
+            get
+            {
+                return _lifetimeScore;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("HighScore must be 0 or greater");
+                }
+                else
+                {
+                    _lifetimeScore = value;
+                }
+            }
+        }
+
+        public double AverageScore
+        {
+            get
+            {
+                return _lifetimeScore / PlayCount;
+            }
+        }
+
+        public string PlayerLevel
+        {
+            get
+            {
+                string output = "";
+                if (_playCount < 10)
+                {
+                    output = "Bronze";
+                }
+                else if (_playCount < 49)
+                {
+                    output = "Silver";
+                }
+                else
+                {
+                    output = "Gold";
+                }
+                return output;
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{_lastName},{_firstName}";
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{_lastName},{_firstName},{_playCount},{_lifetimeScore}";
+        }
+        // Constructors
+
+        /// <summary>
+        /// Creates the player object.
+        /// </summary>
+        /// <param name="first">The first name of the player.</param>
+        /// <param name="last">The last name of the player.</param>
+        /// <param name="plays">The player's lifetime plays.</param>
+        /// <param name="score">The player's lifetime score.</param>
+        public Player(string first, string last, int plays, double score)
+        {
+            FirstName = first;
+            LastName = last;
+            PlayCount = plays;
+            LifetimeScore = score;
+        }
+
+        // Methods
+
+    }
+}
---
+++ PlayerScoreboard/Program.cs
@@ -0,0 +1,107 @@
+﻿using PlayerScoreboard;
+
+/// <summary>
+/// Create a database of players who play towerfall story mode.
+/// Author: Angelo Encarnacion
+/// Last Modified: 2024-12-10
+/// </summary>
+/// 
+
+// Variables
+string firstName;
+string lastName;
+int plays;
+double score;
+
+string option;
+
+// Program intro
+// I'm lazy not doing it in class
+
+// Make sure you loop here or in prompt to get a valid string for names.
+firstName = Prompt("Please enter first name:\t");
+lastName = Prompt("Please enter last name:\t");
+plays = PromptInt("Please enter amount of plays:\t");
+score = PromptDouble("Please enter lifetime score:\t");
+
+Player example = new Player(firstName, lastName, plays, score);
+
+Console.WriteLine();
+Console.WriteLine("\tPlayer Details\t");
+Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Console.WriteLine($"{"Player Name:",20} {example.FirstName} {example.LastName}");
+Console.WriteLine($"{"Play Count:",20} {example.PlayCount}");
+Console.WriteLine($"{"Lifetime Score:",20} {example.LifetimeScore}");
+
+Console.WriteLine();
+option = Prompt("Would you like to see [A]vg score, player [L]evel, or [B]oth:");
+if (option.ToUpper()[0] == 'A')
+{
+    Console.WriteLine($"The average score of the player is: {example.AverageScore}");
+}
+else if (option.ToUpper()[0] == 'L')
+{
+    Console.WriteLine($"The the player level is: {example.PlayerLevel}");
+}
+else if (option.ToUpper()[0] == 'B')
+{
+    Console.WriteLine($"The average score of the player is: {example.AverageScore}");
+    Console.WriteLine($"The the player level is: {example.PlayerLevel}");
+}
+
+
+/// <summary>
+/// Prompts the user, returns a string.
+/// </summary>
+static string Prompt(string messageToUser)
+{
+    Console.Write(messageToUser);
+    return Console.ReadLine();
+}
+
+
+/// <summary>
+/// Prompts the user, looping until a valid int is provided. No rules apply.
+/// </summary>
+static int PromptInt(string message)
+{
+    int output = 0;
+    bool valid = false;
+    do
+    {
+        try
+        {
+            Console.Write(message);
+            output = int.Parse(Console.ReadLine());
+            valid = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Please enter a valid whole number.");
+        }
+    } while (valid == false);
+    return output;
+}
+
+/// <summary>
+/// Prompts the user, looping until a valid double is provided. No rules apply.
+/// </summary>
+static double PromptDouble(string message)
+{
+    double output = 0;
+    bool valid = false;
+    do
+    {
+        try
+        {
+            Console.Write(message);
+            output = double.Parse(Console.ReadLine());
+            valid = true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Please enter a valid whole number.");
+        }
+    } while (valid == false);
+    return output;
+}
